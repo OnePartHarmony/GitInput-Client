7d98da9c6ca91e750a5ab731438de9fa28fb0439
OnePartHarmony <bakingharmony@gmail.com>
2022-10-29 08:52:51 -0400

    created basic api calls for companies and reviews

--- src/api/company.js
+++ src/api/company.js
@@ -13,3 +13,46 @@ export const companyCreate = (company, user) => {
         }
     })
 }
+export const companyIndex = (user) => {
+	return axios({
+		method: 'GET',
+		url: apiUrl + '/companies',
+		headers: {
+            Authorization: `Token token=${user.token}`
+        }
+	})
+}
+
+export const reviewShow = (user, id) => {
+	return axios({
+		method: "GET",
+		url: apiUrl + `/companies/${id}`,
+        headers: {
+            Authorization: `Token token=${user.token}`
+        }		
+	})
+}
+
+export const companyUpdate = (review, user, id) => {	
+	return axios({
+		method: 'PATCH',
+		url: apiUrl + `/companies/${id}`,
+		data: {
+			review: review
+		},
+        headers: {
+            Authorization: `Token token=${user.token}`
+        }
+	})
+}
+
+export const companyDelete = (user, id) => {
+	console.log(id)
+	return axios({
+		method: 'DELETE',
+		url: apiUrl + `/companies/${id}`,
+        headers: {
+            Authorization: `Token token=${user.token}`
+        }
+	})
+}
--- src/api/review.js
+++ src/api/review.js
@@ -13,3 +13,47 @@ export const reviewCreate = (review, user) => {
         }
     })
 }
+
+export const reviewIndex = (user) => {
+	return axios({
+		method: 'GET',
+		url: apiUrl + '/reviews',
+		headers: {
+            Authorization: `Token token=${user.token}`
+        }
+	})
+}
+
+export const reviewShow = (user, id) => {
+	return axios({
+		method: "GET",
+		url: apiUrl + `/reviews/${id}`,
+        headers: {
+            Authorization: `Token token=${user.token}`
+        }		
+	})
+}
+
+export const reviewUpdate = (review, user, id) => {	
+	return axios({
+		method: 'PATCH',
+		url: apiUrl + `/reviews/${id}`,
+		data: {
+			review: review
+		},
+        headers: {
+            Authorization: `Token token=${user.token}`
+        }
+	})
+}
+
+export const reviewDelete = (user, id) => {
+	console.log(id)
+	return axios({
+		method: 'DELETE',
+		url: apiUrl + `/reviews/${id}`,
+        headers: {
+            Authorization: `Token token=${user.token}`
+        }
+	})
+}
