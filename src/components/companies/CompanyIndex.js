09bf4b2930d833c4b5af1e91a9717a2587712834
megwelch <meghanmwelch@gmail.com>
2022-10-31 13:51:33 -0400

    company cards set up in index

--- src/components/companies/CompanyIndex.js
+++ src/components/companies/CompanyIndex.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Form, Button, Container } from 'react-bootstrap'
+import { Form, Button, Container, Card } from 'react-bootstrap'
 
 
 const CompanyIndex = (props) => {
@@ -7,19 +7,97 @@ const CompanyIndex = (props) => {
     
 
     return (
-        <>
-            {/* need to add name, id, value, on change?*/}
-            <div className="company-search-container float-left d-flex align-items-center justify-content-center text-center">
-                <div className="mb-5">
-                    <Form.Control
-                    placeholder="Search a Company"
-                    >
-                    </Form.Control>
-                    <Button className="mt-3">
-                        Search
-                    </Button>
+        <>  
+            <container className="company-index">
+                {/* need to add name, id, value, on change?*/}
+                <div className="company-search-container text-center">
+                    <div className="mb-5 company-search-box">
+                        <section className="m-3 mt-0">Don't see your company listed? Search and add it!</section>
+                        <Form.Control
+                        className="text-center"
+                        placeholder="Search a Company"
+                        >
+                        </Form.Control>
+                        <Button className="mt-3 mb-5">
+                            Search
+                        </Button>
+                    </div>
                 </div>
-            </div>
+                <div className="company-card-container">
+                    <Card className="company-card text-center mb-3">
+                        <h1 class="company-name mt-2">Company Name</h1>
+                        <img className="logo" src="https://logo.clearbit.com/paypal.com"></img>
+                        {/* <a className="mt-2 mb-2 company-link" href="123.com">Company Link</a> */}
+                        <button className="mt-2 mb-2 company-btn">(company name) Website</button>
+                        <p className="company-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        <button className="mb-3 company-btn">View Company Reviews</button>
+                    </Card>
+                    <Card className="company-card text-center mb-3">
+                        <h1 class="company-name mt-2">Company Name</h1>
+                        <img className="logo" src="https://logo.clearbit.com/paypal.com"></img>
+                        {/* <a className="mt-2 mb-2 company-link" href="123.com">Company Link</a> */}
+                        <button className="mt-2 mb-2 company-btn">(company name) Website</button>
+                        <p className="company-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        <button className="mb-3 company-btn">View Company Reviews</button>
+                    </Card>
+                    <Card className="company-card text-center mb-3">
+                        <h1 class="company-name mt-2">Company Name</h1>
+                        <img className="logo" src="https://logo.clearbit.com/paypal.com"></img>
+                        {/* <a className="mt-2 mb-2 company-link" href="123.com">Company Link</a> */}
+                        <button className="mt-2 mb-2 company-btn">(company name) Website</button>
+                        <p className="company-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        <button className="mb-3 company-btn">View Company Reviews</button>
+                    </Card>
+                    <Card className="company-card text-center mb-3">
+                        <h1 class="company-name mt-2">Company Name</h1>
+                        <img className="logo" src="https://logo.clearbit.com/paypal.com"></img>
+                        {/* <a className="mt-2 mb-2 company-link" href="123.com">Company Link</a> */}
+                        <button className="mt-2 mb-2 company-btn">(company name) Website</button>
+                        <p className="company-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        <button className="mb-3 company-btn">View Company Reviews</button>
+                    </Card>
+                    <Card className="company-card text-center mb-3">
+                        <h1 class="company-name mt-2">Company Name</h1>
+                        <img className="logo" src="https://logo.clearbit.com/paypal.com"></img>
+                        {/* <a className="mt-2 mb-2 company-link" href="123.com">Company Link</a> */}
+                        <button className="mt-2 mb-2 company-btn">(company name) Website</button>
+                        <p className="company-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        <button className="mb-3 company-btn">View Company Reviews</button>
+                    </Card>
+                    <Card className="company-card text-center mb-3">
+                        <h1 class="company-name mt-2">Company Name</h1>
+                        <img className="logo" src="https://logo.clearbit.com/paypal.com"></img>
+                        {/* <a className="mt-2 mb-2 company-link" href="123.com">Company Link</a> */}
+                        <button className="mt-2 mb-2 company-btn">(company name) Website</button>
+                        <p className="company-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        <button className="mb-3 company-btn">View Company Reviews</button>
+                    </Card>
+                    <Card className="company-card text-center mb-3">
+                        <h1 class="company-name mt-2">Company Name</h1>
+                        <img className="logo" src="https://logo.clearbit.com/paypal.com"></img>
+                        {/* <a className="mt-2 mb-2 company-link" href="123.com">Company Link</a> */}
+                        <button className="mt-2 mb-2 company-btn">(company name) Website</button>
+                        <p className="company-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        <button className="mb-3 company-btn">View Company Reviews</button>
+                    </Card>
+                    <Card className="company-card text-center mb-3">
+                        <h1 class="company-name mt-2">Company Name</h1>
+                        <img className="logo" src="https://logo.clearbit.com/paypal.com"></img>
+                        {/* <a className="mt-2 mb-2 company-link" href="123.com">Company Link</a> */}
+                        <button className="mt-2 mb-2 company-btn">(company name) Website</button>
+                        <p className="company-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        <button className="mb-3 company-btn">View Company Reviews</button>
+                    </Card>
+                    <Card className="company-card text-center mb-3">
+                        <h1 class="company-name mt-2">Company Name</h1>
+                        <img className="logo" src="https://logo.clearbit.com/paypal.com"></img>
+                        {/* <a className="mt-2 mb-2 company-link" href="123.com">Company Link</a> */}
+                        <button className="mt-2 mb-2 company-btn">(company name) Website</button>
+                        <p className="company-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        <button className="mb-3 company-btn">View Company Reviews</button>
+                    </Card>
+                </div>
+            </container>
         </>
     )
 }
