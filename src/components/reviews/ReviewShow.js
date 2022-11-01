63dd01647d956628b2c87947c4c249dcacc856ab
megwelch <meghanmwelch@gmail.com>
2022-11-01 17:59:58 -0400

    comments component created

--- src/components/reviews/ReviewShow.js
+++ src/components/reviews/ReviewShow.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react' 
+import { useParams, useNavigate } from 'react-router-dom'
 import { Form, Button, Card } from 'react-bootstrap'
+import SetComment from '../comments/SetComment'
 
 const ReviewShow = (props) => {
+
+    const [review, setReview] = useState( null)
+    const { id } = useParams()
+
+
+
 
     return (
         <>
@@ -10,18 +18,25 @@ const ReviewShow = (props) => {
                 <h1 className="text-center mt-5 mb-4">Company Name</h1>
                 <img className="logo-review-show mt-3 mb-5" src="https://logo.clearbit.com/google.com"></img>
                 <h2 className="text-center review-title">Reviews</h2>
-            <container className="review-card">
-                <section className="review-section-1">
-                    <div className="rating-item">User: meg</div>
-                    <div className="rating-item">Rating: *****</div>
-                    <div className="rating-item">Salary: $3,000,000</div>
-                    <div className="rating-item">Starting Position: CEO</div>
-                </section>
-                <section className="review-section-2">
-                    <div className="review-text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
-                </section>
+                <container className="review-card">
+                    <div className="review-info">
+                        <section className="review-section-1">
+                            <div className="rating-item">User: meg</div>
+                            <div className="rating-item">Rating: *****</div>
+                            <div className="rating-item">Salary: $3,000,000</div>
+                            <div className="rating-item">Starting Position: CEO</div>
+                        </section>
+                        <section className="review-section-2">
+                            <div className="review-text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
+                        </section>
+                    </div>
+                    <section className="review-btns">
+                        <button className="review-btn">Comment</button>
+                        <button className="review-btn">Like</button>
+                    </section>
             </container>
             </container>
+            <SetComment></SetComment>
         </>
     )
 }
---
+++ src/components/shared/CommentForm.js
@@ -0,0 +1,18 @@
+import { Form, Button, Container } from 'react-bootstrap'
+
+const CommentForm = () => {
+
+    const { review, handleChange } = props
+
+    return (
+        <>
+            <Form.Control
+                placeholder="enter your comment here"
+                onChange= { handleChange }
+            />
+            <Button type="submit">Submit</Button>
+        </>
+    )
+}
+
+export default CommentForm
