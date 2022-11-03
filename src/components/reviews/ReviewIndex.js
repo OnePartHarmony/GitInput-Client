343b4e44d52dc525c3bb3742fde80d31451b96b3
megwelch <meghanmwelch@gmail.com>
2022-11-03 19:53:57 -0400

    review create styled

--- src/components/reviews/ReviewIndex.js
+++ src/components/reviews/ReviewIndex.js
@@ -26,7 +26,7 @@ const ReviewIndex = (props) => {
 
     const reviewCards = reviews.map(review => {
         return (
-        <Card className="review-index-card" key={review._id} style={{backgroundColor: "#EFEFEF", margin: "20px"}}>
+        <Card className="review-index-card mt-0" key={review._id} style={{backgroundColor: "#EFEFEF", margin: "20px"}}>
                 <h1 className="review-index-heading">
                     <div>{Array.from({length: review.generalRating}, (a,index) => 
                         <svg key={index} width="30" height="30" viewBox="0 0 60 60"><path fill="gold" d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z"/></svg>
@@ -62,7 +62,7 @@ const ReviewIndex = (props) => {
         <div className="mt-5" style={{flex: 1, textAlign: "center", }}>
             
             <div  className="company-profile" style={{width: "33vw", margin: "auto", padding: "10px", position: "relative"}}>
-                <div className="title-box fixed">
+                <div className="title-box fixed mb-3">
                     <h3 className="profile-title fixed">Reviews</h3> 
                 </div>      
                 <div className="scroll">        
